88fecebaf110c07ee9f5411e464574fda65137ef
Ronit3000 <ronitmaurya123@gmail.com>
2026-09-14 18:05:17 +0530

    Redirect root to listings

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ const sessionOptions = {
     }
 }
 
-app.get("/", (req,res)=> {
-    res.send("Hi! I am root");
+app.get("/", (req, res) => {
+    res.redirect("/listings");
 });
 
 app.use(session(sessionOptions));
